63e69833b01775bbf3ad6d8f8caddd1e5216a760
Szarek <szarek@o2.pl>
2024-03-03 16:18:30 +0100

    logger correction

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ from .manager import Manager
 logger = logging.getLogger(__name__)
 FORMAT = "%(asctime)s [%(filename)s][%(funcName)s:%(lineno)s][%(levelname)s]: %(message)s"
 logFilePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "auto_new_adjuster.log")
-file_handler = RotatingFileHandler(logFilePath, maxBytes=1e6, backupCount=3)
+file_handler = RotatingFileHandler(logFilePath, maxBytes=10 * 1024, backupCount=3)
 formatter = logging.Formatter(FORMAT)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
@@ -18,8 +18,8 @@ def sync_did_finish():
     logger.info("#")
     logger.info("####################################################################################")
     logger.info("#")
-    manager: Manager = Manager(logger)
-    manager.update()
+    m1: Manager = Manager(logger)
+    m1.update()
     logger.debug(os.path.abspath(__file__))
 
 
