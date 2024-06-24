b4e3a252ec7bc58a101cd1c23b4914817ec8e104
Szarek <szarek@o2.pl>
2024-06-24 18:03:13 +0200

    Deck make decisions

--- deck.py
+++ deck.py
@@ -2,16 +2,20 @@ from aqt import mw
 from collections.abc import Sequence
 import logging
 from .config import DeckConfig
+from time import time
 
 
 class Deck:
-    def __init__(self, deck_id: str, young_difficulty_max: int, logger: logging) -> None:
+    def __init__(self, deck_id: str, young_difficulty_max: int, logger: logging, last_updated: int) -> None:
         self.logger: logging = logger
         self.raw_data: dict[str, str] = mw.col.decks.get(deck_id)
         self.young_difficulty_max: int = young_difficulty_max
+        self.last_updated = last_updated
         self.id: str = str(self.raw_data["id"])
         self.name = self.raw_data["name"]
         self.deck_config: DeckConfig = DeckConfig(logger=self.logger, deck_id=self.id)
+        self.low_young_difficulty_max = 50
+        self.high_young_difficulty_max = 150
 
     def get_count_cards_introduced_today(self) -> int:
         query = f"deck:{self.name} introduced:1"
@@ -53,3 +57,38 @@ class Deck:
             return round(1.0 - (all_again_cards_count / all_cards_count), 2)
         else:
             return 0.9
+
+    def set_new_cards_count(self):
+        deck_young_difficulty_sum: int = round(self.get_deck_young_difficulty_sum())
+        deck_count_cards_introduced_today: int = self.get_count_cards_introduced_today()
+        deck_new_config_limit: int = 0
+        if self.get_count_still_in_queue() == 0:
+            self.set_new_deck_difficulty()
+            deck_new_config_limit = max(0,
+                                        self.young_difficulty_max - deck_young_difficulty_sum + deck_count_cards_introduced_today)
+
+        self.deck_config.set_new_count(new_count=deck_new_config_limit)
+
+        debug_message: str = f"[{self.deck_config.name}][{self.name}] "
+        debug_message += f"deck_young_difficulty_max {self.young_difficulty_max} | "
+        debug_message += f"deck_young_difficulty_sum {deck_young_difficulty_sum} | "
+        debug_message += f"deck_count_cards_introduced_today {deck_count_cards_introduced_today}"
+        self.logger.debug(debug_message)
+
+    def set_new_deck_difficulty(self):
+        # self.logger.debug(f"[{deck.name}] young_max_difficulty: {deck.young_max_difficulty}")
+        if time() - self.last_updated <= 20 * 60 * 60:
+            # self.logger.debug(f"[{deck.name}] Updated in last 20h - no action is needed.")
+            return
+        current_value = self.young_difficulty_max
+        todays_again_hit = self.get_todays_again_hit()
+        debug_message: str = f"[{self.name}] Today again hit: {round(todays_again_hit * 100)}% | "
+        debug_message += f"current young_difficulty_max: {current_value} | "
+        if todays_again_hit >= 0.95:
+            current_value = min(self.high_young_difficulty_max, current_value + 1)
+        if todays_again_hit < 0.85:
+            current_value = max(self.low_young_difficulty_max, current_value - 1)
+        debug_message += f"new young_difficulty_max: {current_value}"
+        self.logger.debug(debug_message)
+        self.last_updated = int(time())
+        self.young_difficulty_max = current_value
--- manager.py
+++ manager.py
@@ -1,7 +1,6 @@
 from aqt import mw
 from .deck import Deck
 import logging
-from time import time
 
 
 class Manager:
@@ -11,15 +10,14 @@ class Manager:
         self.raw_decks: list = mw.col.decks.all()
         self.update_add_on_config()
         self.decks: set[Deck] = self.get_decks()
-        self.low_young_difficulty_max = 50
-        self.high_young_difficulty_max = 150
 
     def __del__(self) -> None:
         mw.addonManager.writeConfig(__name__, self.raw_add_on_config)
 
     def update(self) -> None:
         for deck in self.decks:
-            self.set_new_cards_count(deck=deck)
+            deck.set_new_deck_difficulty()
+            deck.set_new_cards_count()
 
     def update_add_on_config(self):
         if "decks" not in self.raw_add_on_config:
@@ -39,45 +37,10 @@ class Manager:
         for raw_deck in self.raw_decks:
             deck_id: str = str(raw_deck["id"])
             if deck_id in self.raw_add_on_config["decks"] and self.raw_add_on_config["decks"][deck_id]["enabled"]:
+                young_difficulty_max = self.raw_add_on_config["decks"][deck_id]["young_max_difficulty"]
+                last_updated = self.raw_add_on_config["decks"][deck_id]["last_updated"]
                 deck = Deck(deck_id=deck_id,
-                            young_difficulty_max=self.raw_add_on_config["decks"][deck_id]["young_max_difficulty"],
-                            logger=self.logger)
+                            young_difficulty_max=young_difficulty_max,
+                            logger=self.logger, last_updated=last_updated)
                 decks_set.add(deck)
         return decks_set
-
-    def set_new_deck_difficulty(self, deck: Deck):
-        # self.logger.debug(f"[{deck.name}] young_max_difficulty: {deck.young_max_difficulty}")
-        if time() - self.raw_add_on_config["decks"][deck.id]["last_updated"] <= 20 * 60 * 60:
-            # self.logger.debug(f"[{deck.name}] Updated in last 20h - no action is needed.")
-            return
-        current_value = self.raw_add_on_config["decks"][deck.id]["young_max_difficulty"]
-        todays_again_hit = deck.get_todays_again_hit()
-        debug_message: str = f"[{deck.name}] Today again hit: {round(todays_again_hit * 100)}% | "
-        debug_message += f"current young_difficulty_max: {current_value} | "
-        if todays_again_hit >= 0.95:
-            current_value = min(self.high_young_difficulty_max, current_value + 1)
-        if todays_again_hit < 0.85:
-            current_value = max(self.low_young_difficulty_max, current_value - 1)
-        debug_message += f"new young_difficulty_max: {current_value}"
-        self.logger.debug(debug_message)
-        self.raw_add_on_config["decks"][deck.id]["last_updated"] = int(time())
-        self.raw_add_on_config["decks"][deck.id]["young_max_difficulty"] = current_value
-        deck.young_difficulty_max = current_value
-        # self.logger.debug(f"[{deck.name}] New young_max_difficulty: {deck.young_max_difficulty}")
-
-    def set_new_cards_count(self, deck: Deck):
-        deck_young_difficulty_sum: int = round(deck.get_deck_young_difficulty_sum())
-        deck_count_cards_introduced_today: int = deck.get_count_cards_introduced_today()
-        deck_new_config_limit: int = 0
-        if deck.get_count_still_in_queue() == 0:
-            self.set_new_deck_difficulty(deck)
-            deck_new_config_limit = max(0,
-                                        deck.young_difficulty_max - deck_young_difficulty_sum + deck_count_cards_introduced_today)
-
-        deck.deck_config.set_new_count(new_count=deck_new_config_limit)
-
-        debug_message: str = f"[{deck.deck_config.name}][{deck.name}] "
-        debug_message += f"deck_young_difficulty_max {deck.young_difficulty_max} | "
-        debug_message += f"deck_young_difficulty_sum {deck_young_difficulty_sum} | "
-        debug_message += f"deck_count_cards_introduced_today {deck_count_cards_introduced_today}"
-        self.logger.debug(debug_message)
