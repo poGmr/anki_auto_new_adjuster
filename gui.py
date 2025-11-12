6213ad5b6433157efb43c96d727f140b6d6261c0
Szarek <szarek@o2.pl>
2025-11-12 18:54:12 +0100

    GUI - columns rename

--- gui.py
+++ gui.py
@@ -4,6 +4,16 @@ import logging
 from .addon_config import AddonConfig
 
 
+class DC:
+    NAME = 0
+    ENABLED = 1
+    TODAYS_NLRY = 2
+    TODAYS_NLRY_MAX = 3
+    ALL_NLRY = 4
+    NEW_DONE = 5
+    STATUS = 6
+
+
 def get_checkbox_css_style() -> str:
     return """
                         QCheckBox::indicator {
@@ -70,24 +80,29 @@ class GUI:
     def get_deck_settings_group_box(self):
         decks_grid_layout = QGridLayout()
         decks_grid_layout.setContentsMargins(5, 5, 5, 5)
-        decks_grid_layout.addWidget(get_header_table_ql("DECK"), 0, 0, alignment=Qt.AlignmentFlag.AlignCenter)
-        decks_grid_layout.addWidget(get_header_table_ql("ENABLED"), 0, 1, alignment=Qt.AlignmentFlag.AlignCenter)
-        decks_grid_layout.addWidget(get_header_table_ql("TODAY'S\nNLRY"), 0, 2, alignment=Qt.AlignmentFlag.AlignCenter)
-        decks_grid_layout.addWidget(get_header_table_ql("TODAY'S\nNLRY MAX"), 0, 3,
+        decks_grid_layout.addWidget(get_header_table_ql("NAME"), 0, DC.NAME,
                                     alignment=Qt.AlignmentFlag.AlignCenter)
-        decks_grid_layout.addWidget(get_header_table_ql("ALL\nNLRY"), 0, 4,
+        decks_grid_layout.addWidget(get_header_table_ql("ENABLED"), 0, DC.ENABLED,
                                     alignment=Qt.AlignmentFlag.AlignCenter)
-        decks_grid_layout.addWidget(get_header_table_ql("NEW\nDONE"), 0, 5, alignment=Qt.AlignmentFlag.AlignCenter)
-        decks_grid_layout.addWidget(get_header_table_ql("STATUS"), 0, 6, alignment=Qt.AlignmentFlag.AlignCenter)
+        decks_grid_layout.addWidget(get_header_table_ql("TODAY'S\nNLRY"), 0, DC.TODAYS_NLRY,
+                                    alignment=Qt.AlignmentFlag.AlignCenter)
+        decks_grid_layout.addWidget(get_header_table_ql("TODAY'S\nNLRY MAX"), 0, DC.TODAYS_NLRY_MAX,
+                                    alignment=Qt.AlignmentFlag.AlignCenter)
+        decks_grid_layout.addWidget(get_header_table_ql("ALL\nNLRY"), 0, DC.ALL_NLRY,
+                                    alignment=Qt.AlignmentFlag.AlignCenter)
+        decks_grid_layout.addWidget(get_header_table_ql("NEW\nDONE"), 0, DC.NEW_DONE,
+                                    alignment=Qt.AlignmentFlag.AlignCenter)
+        decks_grid_layout.addWidget(get_header_table_ql("STATUS"), 0, DC.STATUS,
+                                    alignment=Qt.AlignmentFlag.AlignCenter)
 
-        i = 1
+        row = 1
         all_nlry_count = 0
         today_nlry_sum = 0
         today_nlry_max_sum = 0
         for did in self.add_on_config.get_decks_ids():
             ####################################################################################################
             deck_name = QLabel(self.add_on_config.get_deck_state(did=did, key="name"))
-            decks_grid_layout.addWidget(deck_name, i, 0, alignment=Qt.AlignmentFlag.AlignLeft)
+            decks_grid_layout.addWidget(deck_name, row, DC.NAME, alignment=Qt.AlignmentFlag.AlignLeft)
             ####################################################################################################
             enabled = QCheckBox()
             enabled.setProperty("did", did)
@@ -95,15 +110,15 @@ class GUI:
             enabled.setChecked(self.add_on_config.get_deck_state(did=did, key="enabled"))
             enabled.stateChanged.connect(
                 lambda state, checkbox=enabled: self.enable_checkbox_change_state(state, checkbox))
-            decks_grid_layout.addWidget(enabled, i, 1, alignment=Qt.AlignmentFlag.AlignCenter)
+            decks_grid_layout.addWidget(enabled, row, DC.ENABLED, alignment=Qt.AlignmentFlag.AlignCenter)
             if not enabled.isChecked():
-                i += 1
+                row += 1
                 continue
             ####################################################################################################
             today_nlry = int(self.add_on_config.get_deck_state(did=did, key="todays_nlry_sum"))
             today_nlry_sum += today_nlry
             todays_nlry_sum = QLabel(str(today_nlry))
-            decks_grid_layout.addWidget(todays_nlry_sum, i, 2,
+            decks_grid_layout.addWidget(todays_nlry_sum, row, DC.TODAYS_NLRY,
                                         alignment=Qt.AlignmentFlag.AlignCenter)
             ####################################################################################################
             todays_nlry_max = str(
@@ -116,27 +131,32 @@ class GUI:
             todays_nlry_max_spin.setProperty("did", did)
             todays_nlry_max_spin.valueChanged.connect(
                 lambda value, spin=todays_nlry_max_spin: self.todays_spin_update_value(value, spin))
-            decks_grid_layout.addWidget(todays_nlry_max_spin, i, 3,
+            decks_grid_layout.addWidget(todays_nlry_max_spin, row, DC.TODAYS_NLRY_MAX,
                                         alignment=Qt.AlignmentFlag.AlignCenter)
             ####################################################################################################
             nlry_sum = QLabel(
                 str(self.add_on_config.get_deck_state(did=did, key="nlry_sum")))
             all_nlry_count += int(nlry_sum.text())
-            decks_grid_layout.addWidget(nlry_sum, i, 4, alignment=Qt.AlignmentFlag.AlignCenter)
+            decks_grid_layout.addWidget(nlry_sum, row, DC.ALL_NLRY, alignment=Qt.AlignmentFlag.AlignCenter)
             ####################################################################################################
             new_done = QLabel(str(self.add_on_config.get_deck_state(did=did, key="new_done")))
-            decks_grid_layout.addWidget(new_done, i, 5, alignment=Qt.AlignmentFlag.AlignCenter)
+            decks_grid_layout.addWidget(new_done, row, DC.NEW_DONE, alignment=Qt.AlignmentFlag.AlignCenter)
             ####################################################################################################
             status_text = str(self.add_on_config.get_deck_state(did=did, key="status"))
             status_label = QLabel(status_text)
-            decks_grid_layout.addWidget(status_label, i, 6, alignment=Qt.AlignmentFlag.AlignCenter)
+            decks_grid_layout.addWidget(status_label, row, DC.STATUS, alignment=Qt.AlignmentFlag.AlignCenter)
             ####################################################################################################
-            i += 1
-        # ADD SUMMARY LINE
-        decks_grid_layout.addWidget(QLabel("TOTAL"), i, 0, alignment=Qt.AlignmentFlag.AlignLeft)
-        decks_grid_layout.addWidget(QLabel(str(today_nlry_sum)), i, 2, alignment=Qt.AlignmentFlag.AlignCenter)
-        decks_grid_layout.addWidget(QLabel(str(today_nlry_max_sum)), i, 3, alignment=Qt.AlignmentFlag.AlignCenter)
-        decks_grid_layout.addWidget(QLabel(str(all_nlry_count)), i, 4, alignment=Qt.AlignmentFlag.AlignCenter)
+            row += 1
+        # ADD SUMMARY LINE #################################################################################
+        decks_grid_layout.addWidget(QLabel("TOTAL"), row, DC.NAME,
+                                    alignment=Qt.AlignmentFlag.AlignLeft)
+        decks_grid_layout.addWidget(QLabel(str(today_nlry_sum)), row, DC.TODAYS_NLRY,
+                                    alignment=Qt.AlignmentFlag.AlignCenter)
+        decks_grid_layout.addWidget(QLabel(str(today_nlry_max_sum)), row, DC.TODAYS_NLRY_MAX,
+                                    alignment=Qt.AlignmentFlag.AlignCenter)
+        decks_grid_layout.addWidget(QLabel(str(all_nlry_count)), row, DC.ALL_NLRY,
+                                    alignment=Qt.AlignmentFlag.AlignCenter)
+        ####################################################################################################
         decks_group_box = QGroupBox("DECKS")
         decks_group_box.setLayout(decks_grid_layout)
         return decks_group_box
